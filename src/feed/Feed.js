cb2661a510b420a0a5a912305a10facc9f06a414
Dave T <105549233+Dmum303@users.noreply.github.com>
2023-03-02 10:18:34 +0000

    changing fetch fucntion

--- src/feed/Feed.js
+++ src/feed/Feed.js
@@ -9,19 +9,34 @@ const Feed = ({ navigate }) => {
   //usestate always returns an array with 2 values
   // use the second to set the info in the first, to be used later
 
-  const loadApi = () => {
-    fetch(
-      'https://content.guardianapis.com/search?q=&query-fields=headline&show-fields=thumbnail,headline,byline&order-by=newest&api-key=33c178d6-be74-4321-93e7-ec61c0ac7723'
-    )
-      //'https://www.boredapi.com/api/activity')
-      .then((response) => response.json())
-      .then(async (data) => {
-        console.log(data);
-        console.log(data.response.results);
-        //This should set the key from api results to the api var
-        setApi(data.response.results);
-        // console.log(api);
-      });
+  // const loadApi = () => {
+  //   fetch(
+  //     'https://content.guardianapis.com/search?q=&query-fields=headline&show-fields=thumbnail,headline,byline&order-by=newest&api-key=33c178d6-be74-4321-93e7-ec61c0ac7723'
+  //   )
+  //     //'https://www.boredapi.com/api/activity')
+  //     .then((response) => response.json())
+  //     .then(async (data) => {
+  //       console.log(data);
+  //       console.log(data.response.results);
+  //       //This should set the key from api results to the api var
+  //       setApi(data.response.results);
+  //       // console.log(api);
+  //     });
+  // };
+
+  const loadApi = async () => {
+    try {
+      const response = await fetch(
+        'https://content.guardianapis.com/search?q=&query-fields=headline&show-fields=thumbnail,headline,byline&order-by=newest&api-key=33c178d6-be74-4321-93e7-ec61c0ac7723'
+      );
+      const data = await response.json();
+      console.log(data);
+      console.log(data.response.results);
+      //This should set the key from api results to the api var
+      setApi(data.response.results);
+    } catch (error) {
+      console.log('Error fetching API data: ', error);
+    }
   };
 
   // calls the loadApi, I think it auto reloads page if any changes but it is simple sofar
@@ -36,23 +51,6 @@ const Feed = ({ navigate }) => {
         <WebsiteLogo />
         <NavBar />
         <HomePage apiData={api} />
-        {/* <ul>
-          {api.map((story) => (
-            <div key={story.id}>
-              <h2>{story.sectionName}</h2>
-              <br></br>
-              <p>{story.webTitle}</p>
-
-              <br></br>
-              <img
-                src={story.fields.thumbnail}
-                alt={story.fields.headline}
-              ></img>
-              <br></br>
-              <a href={story.webUrl}>{story.sectionName}</a>
-            </div>
-          ))}
-        </ul> */}
       </div>
     </>
   );
